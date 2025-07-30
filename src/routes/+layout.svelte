5f47ba14087c5725129315b12550168eca02d768
Johann Dirschl <johann.dirschl@gmx.de>
2025-07-30 22:21:25 +0200

    Backup vor NewsFlash SEO Implementation - Favicon Optimierung und Keywords serverseitig

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -115,7 +115,6 @@
 <svelte:head>
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <meta charset="UTF-8" />
-  <link rel="icon" type="image/png" href="/culoca-icon.png" />
 </svelte:head>
 
 <div class="app">
