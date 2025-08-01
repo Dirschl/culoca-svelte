26b533fb558f3e2c49335139d48391965d741c57
Johann Dirschl <johann.dirschl@gmx.de>
2025-08-01 08:24:29 +0200

    Fix: Import-Pfade mit .js-Extension und entferne doppelte Definitionen

--- src/routes/api/admin/auth-users/+server.ts
+++ src/routes/api/admin/auth-users/+server.ts
@@ -1,5 +1,5 @@
 import { json } from '@sveltejs/kit';
-import { supabaseAdmin } from '$lib/supabaseAdmin';
+import { supabaseAdmin } from '$lib/supabaseAdmin.js';
 
 export const GET = async () => {
   try {
