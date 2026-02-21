8a93e89dada075501830c028bfa2449926d37b45
Johann Dirschl <johann.dirschl@gmx.de>
2026-02-21 18:41:18 +0100

    Fix Vercel build runtime to Node 20

--- svelte.config.js
+++ svelte.config.js
@@ -7,7 +7,7 @@ const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() },
+	kit: { adapter: adapter({ runtime: 'nodejs20.x' }) },
 	// compilerOptions: { runes: true }  // Entfernt für Svelte 4
 };
 
